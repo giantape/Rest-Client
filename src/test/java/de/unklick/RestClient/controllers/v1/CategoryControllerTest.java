f48d25a9de02d28bafbdbe78e5bcb88ded37fe91
sapinodejs@gmail.com <>
2020-03-31 20:42:24 +0200

    refactoring base_url

--- src/test/java/de/unklick/RestClient/controllers/v1/CategoryControllerTest.java
+++ src/test/java/de/unklick/RestClient/controllers/v1/CategoryControllerTest.java
@@ -56,7 +56,7 @@ public class CategoryControllerTest {
 
         when(categoryService.getAllCategories()).thenReturn(categories);
 
-        mockMvc.perform(get("/api/v1/categories")
+        mockMvc.perform(get(CategoryController.BASE_URL)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.categories", hasSize(2)));
@@ -70,7 +70,7 @@ public class CategoryControllerTest {
 
         when(categoryService.getCategoryByName(anyString())).thenReturn(category1);
 
-        mockMvc.perform(get("/api/v1/categories/Giant")
+        mockMvc.perform(get(CategoryController.BASE_URL +"/Giant")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", equalTo(NAME)));
